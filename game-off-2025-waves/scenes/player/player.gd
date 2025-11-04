d6ee25ef746bbdf4293157a53af11794b9a1432c
Nademtis <mathias.99.madsen@gmail.com>
2025-11-04 21:17:47 +0100

    wip on more basic setup

---
+++ game-off-2025-waves/scenes/player/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const MOVESPEED : float = 2500
+
+func _physics_process(delta: float) -> void:
+	var direction : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#print(direction)
+	
+	if direction != Vector2.ZERO:
+		velocity = direction * MOVESPEED * delta
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
