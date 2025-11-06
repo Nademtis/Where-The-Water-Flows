f9969e864a7ccfcd42084a9a53a788b4aea5d710
Nademtis <mathias.99.madsen@gmail.com>
2025-11-06 16:00:20 +0100

    water works but is hardcoded

--- game-off-2025-waves/scenes/player/player.gd
+++ game-off-2025-waves/scenes/player/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 const MOVESPEED : float = 2500
 
 func _physics_process(delta: float) -> void:
-	var direction : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	#var direction : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction : Vector2 = Input.get_vector("left", "right", "up", "down")
 	#print(direction)
 	
 	if direction != Vector2.ZERO:
--- game-off-2025-waves/tiles/waterController.gd
+++ game-off-2025-waves/tiles/waterController.gd
@@ -3,7 +3,7 @@ extends TileMapLayer
 #waterController-gd
 #this class is responsible for the water going up
 
-@export var water_level : float = 1
+@export var water_level : float = 1.0
 
 @onready var height_map: TileMapLayer = $"../heightMap" # defines the height on the tiles - tiles are drawn on top of the walkable tilemaplayer
 # height is defines as a cumstom data layer
@@ -14,17 +14,24 @@ const HEIGHT_LAYER_NAME := "height"
 const WATER_TYPE_LAYER_NAME := "water_type"
 
  # the atlas coordinates for the correct FULL water tile
-const WATER_TILE_UP_INDEX : Vector2i = Vector2i(0, 0)
-const WATER_TILE_RIGHT_INDEX : Vector2i = Vector2i(2, 0)
-const WATER_TILE_LEFT_INDEX : Vector2i = Vector2i(3, 0)
-const WATER_TILE_ALL_INDEX : Vector2i = Vector2i(4, 0)
-const WATER_TILE_LEFT_CORNER_INDEX : Vector2i = Vector2i(5, 0)
-const WATER_TILE_RIGHT_CORNER_INDEX : Vector2i = Vector2i(6, 0)
+const WATER_TILE_UP : Vector2i = Vector2i(0, 0)
+const WATER_TILE_RIGHT : Vector2i = Vector2i(2, 0)
+const WATER_TILE_LEFT : Vector2i = Vector2i(3, 0)
+const WATER_TILE_ALL : Vector2i = Vector2i(4, 0)
+const WATER_TILE_LEFT_CORNER : Vector2i = Vector2i(5, 0)
+const WATER_TILE_RIGHT_CORNER : Vector2i = Vector2i(6, 0)
 
+#corner animation tiles
+#const WATER_TILE_UP_ANIMATION_INDEX : Vector2i = Vector2i(0, 0)
+const WATER_TILE_RIGHT_ANIMATION : Vector2i = Vector2i(0, 1)
+const WATER_TILE_LEFT_ANIMATION : Vector2i = Vector2i(0, 2)
+const WATER_TILE_ALL_ANIMATION : Vector2i = Vector2i(0, 3)
+const WATER_TILE_LEFT_CORNER_ANIMATION : Vector2i = Vector2i(0, 4)
+const WATER_TILE_RIGHT_CORNER_ANIMATION : Vector2i = Vector2i(0, 5)
 
 
 func _ready() -> void:
-	#update_water_tiles()
+	update_water_tiles()
 	pass
 
 func _process(_delta: float) -> void:
@@ -34,25 +41,49 @@ func _process(_delta: float) -> void:
 
 func update_water_tiles():
 	clear() # remove old water tiles before redrawing
+	print_debug("water level: ", water_level)
 	for cell : Vector2i in height_map.get_used_cells():
 		var data :TileData = height_map.get_cell_tile_data(cell)
 		var cell_height : float = data.get_custom_data(HEIGHT_LAYER_NAME)
 		var cell_water_type : String = data.get_custom_data(WATER_TYPE_LAYER_NAME)
 		#print(cell_height)
-		if cell_height <= water_level:
-			_set_water_tile(cell, cell_water_type)
-
+		#if cell_height < water_level:
+			#print("animate level: ", cell_height)
+			#_set_animation_corner_water_tile(cell, cell_water_type)
+		#elif cell_height == water_level:
+			#print("full water: ", cell_height)
+			#_set_full_water_tile(cell, cell_water_type)
+		if water_level == 1.0:
+			if cell_height == 1: _set_animation_corner_water_tile(cell, cell_water_type)
 		
+		elif water_level == 2.0:
+			if cell_height < 2.0: _set_full_water_tile(cell, cell_water_type)
+			if cell_height == 2.0: _set_animation_corner_water_tile(cell, cell_water_type)
+			
+		elif water_level == 3.0:
+			if cell_height < 3.0: _set_full_water_tile(cell, cell_water_type)
+			if cell_height == 3.0: _set_animation_corner_water_tile(cell, cell_water_type)
 
-func _set_water_tile(cell : Vector2i, water_type : String) -> void:
+func _set_full_water_tile(cell : Vector2i, water_type : String) -> void:
 	match water_type:
-		"up": set_cell(cell, 0, WATER_TILE_UP_INDEX)
-		"right": set_cell(cell, 0, WATER_TILE_RIGHT_INDEX)
-		"left": set_cell(cell, 0, WATER_TILE_LEFT_INDEX)
-		"all": set_cell(cell, 0, WATER_TILE_ALL_INDEX)
-		"stair": set_cell(cell, 0, WATER_TILE_ALL_INDEX)
-		"left_corner": set_cell(cell, 0, WATER_TILE_LEFT_CORNER_INDEX)
-		"right_corner": set_cell(cell, 0, WATER_TILE_RIGHT_CORNER_INDEX)
+		"up": set_cell(cell, 0, WATER_TILE_UP)
+		"right": set_cell(cell, 0, WATER_TILE_RIGHT)
+		"left": set_cell(cell, 0, WATER_TILE_LEFT)
+		"all": set_cell(cell, 0, WATER_TILE_ALL)
+		"stair": set_cell(cell, 0, WATER_TILE_ALL)
+		"left_corner": set_cell(cell, 0, WATER_TILE_LEFT_CORNER)
+		"right_corner": set_cell(cell, 0, WATER_TILE_RIGHT_CORNER)
+		_: printerr("water type not defined")
+		
+func _set_animation_corner_water_tile(cell : Vector2i, water_type : String) -> void:
+	match water_type:
+		"up": pass
+		"right": set_cell(cell, 0, WATER_TILE_RIGHT_ANIMATION)
+		"left": set_cell(cell, 0, WATER_TILE_LEFT_ANIMATION)
+		"all": set_cell(cell, 0, WATER_TILE_ALL_ANIMATION)
+		"stair": set_cell(cell, 0, WATER_TILE_ALL_ANIMATION)
+		"left_corner": set_cell(cell, 0, WATER_TILE_LEFT_CORNER_ANIMATION)
+		"right_corner": set_cell(cell, 0, WATER_TILE_RIGHT_CORNER_ANIMATION)
 		_: printerr("water type not defined")
 
 func _on_v_slider_value_changed(value: float) -> void:
