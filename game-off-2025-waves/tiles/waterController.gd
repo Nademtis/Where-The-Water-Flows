817ad2434d1271f3ac066438b40951948cac0525
Nademtis <mathias.99.madsen@gmail.com>
2025-11-07 19:22:46 +0100

    wip water anim

--- game-off-2025-waves/tiles/waterController.gd
+++ game-off-2025-waves/tiles/waterController.gd
@@ -33,8 +33,13 @@ const WATER_TILE_ALL_ANIMATION : Vector2i = Vector2i(0, 3)
 const WATER_TILE_LEFT_CORNER_ANIMATION : Vector2i = Vector2i(0, 4)
 const WATER_TILE_RIGHT_CORNER_ANIMATION : Vector2i = Vector2i(0, 5)
 
+var sorted_cells : Array[Vector2i] = []
 
 func _ready() -> void:
+	var cells : Array[Vector2i] = height_map.get_used_cells()
+	cells.sort_custom(func(a, b): return a.y > b.y)
+	sorted_cells = cells
+	
 	update_water_tiles()
 	Events.connect("water_level_direction", _update_water_level)
 	pass
@@ -52,17 +57,26 @@ func _update_water_level(going_up : bool) -> void:
 	update_water_tiles()
 
 func update_water_tiles():
+	var last_y := INF
 	clear()
-	for cell : Vector2i in height_map.get_used_cells():
+	for cell : Vector2i in sorted_cells:
 		var data : TileData = height_map.get_cell_tile_data(cell)
 		var cell_height : float = data.get_custom_data(HEIGHT_LAYER_NAME)
 		var cell_water_type : String = data.get_custom_data(WATER_TYPE_LAYER_NAME)
 
+		#don't anim if water level is only 1, since that's the buttom
 		if water_level == 1.0:
 			if cell_height == 1: _set_animation_corner_water_tile(cell, cell_water_type)
 		else:
-			if cell_height < water_level: _set_full_water_tile(cell, cell_water_type)
+			if cell_height < water_level:
+				_set_full_water_tile(cell, cell_water_type)
+				if cell.y < last_y:
+					last_y = cell.y
+					await get_tree().create_timer(0.05).timeout
 			if cell_height == water_level: _set_animation_corner_water_tile(cell, cell_water_type)
+		
+		#wait for the water when stepping up in level
+
 
 func _set_full_water_tile(cell : Vector2i, water_type : String) -> void:
 	match water_type:
