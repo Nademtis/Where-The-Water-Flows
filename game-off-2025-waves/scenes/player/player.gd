cfcd95545552b6e5e4f1afe715a2afd0369bb1ea
Nademtis <mathias.99.madsen@gmail.com>
2025-11-08 22:42:07 +0100

    new player controller

--- game-off-2025-waves/scenes/player/player.gd
+++ game-off-2025-waves/scenes/player/player.gd
@@ -2,6 +2,15 @@ extends CharacterBody2D
 
 const MOVESPEED : float = 65
 
+@export var max_speed: float = 65.0
+@export var acceleration: float = 400.0
+@export var deceleration: float = 1000
+
+# 2D input direction before isometric transform
+var input_dir: Vector2
+#var iso_dir: Vector2
+var move_dir: Vector2
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("water_up"):
 		move_water(true)
@@ -9,17 +18,51 @@ func _process(_delta: float) -> void:
 		move_water(false)
 
 
-func _physics_process(_delta: float) -> void:
-	#var direction : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction : Vector2 = Input.get_vector("left", "right", "up", "down")
-	#print(direction)
-	
-	if direction != Vector2.ZERO:
-		velocity = direction * MOVESPEED
+func _physics_process(delta: float) -> void:
+	input_dir = Input.get_vector("left", "right", "up", "down")
+
+	if input_dir != Vector2.ZERO:
+		var dir_name := _get_direction_name(input_dir)
+
+		match dir_name:
+			#"northeast":
+				#move_dir = Vector2(1, -0.5).normalized()
+			#"northwest":
+				#move_dir = Vector2(-1, -0.5).normalized()
+			#"southeast":
+				#move_dir = Vector2(1, 0.5).normalized()
+			#"southwest":
+				#move_dir = Vector2(-1, 0.5).normalized()
+			_: # top-down directions
+				move_dir = input_dir
+
+		velocity = velocity.move_toward(move_dir * max_speed, acceleration * delta)
 	else:
-		velocity = Vector2.ZERO
-	
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+
 	move_and_slide()
 
 func move_water(is_up : bool) -> void:
 	Events.emit_signal("requested_water_level_direction", is_up)
+	
+
+func _get_direction_name(v: Vector2) -> String:
+	# 8-direction classification
+	if v.x == 0 and v.y < 0:
+		return "north"
+	elif v.x == 0 and v.y > 0:
+		return "south"
+	elif v.x < 0 and v.y == 0:
+		return "west"
+	elif v.x > 0 and v.y == 0:
+		return "east"
+	elif v.x > 0 and v.y < 0:
+		return "northeast"
+	elif v.x < 0 and v.y < 0:
+		return "northwest"
+	elif v.x > 0 and v.y > 0:
+		return "southeast"
+	elif v.x < 0 and v.y > 0:
+		return "southwest"
+	else:
+		return "idle"
