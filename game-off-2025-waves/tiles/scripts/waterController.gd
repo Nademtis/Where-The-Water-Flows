788b2f6efbec55b398b3aba004775e591463e755
Nademtis <mathias.99.madsen@gmail.com>
2025-11-10 20:43:07 +0100

    wip everything. im dying

--- game-off-2025-waves/tiles/scripts/waterController.gd
+++ game-off-2025-waves/tiles/scripts/waterController.gd
@@ -12,7 +12,10 @@ var previous_level : float = 1.0
 #animation speed / wait time for specefied tiles. should probably refactor
 @export var water_animation_wait_time : float = 0.001
 
-@onready var height_map: TileMapLayer = $"../heightMap" # this defines the height on the tiles - tiles are drawn on top of the walkable tilemaplayer
+#@onready var height_map: TileMapLayer = $"../heightMap" # this defines the height on the tiles - tiles are drawn on top of the walkable tilemaplayer
+@onready var height_map: TileMapLayer = $"../../../heightMap"
+
+
 # height and water_type is defines as a cumstom data layer in height_map
 
 var sorted_cells : Array[Vector2i] = []
