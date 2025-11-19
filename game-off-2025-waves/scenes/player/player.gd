84a91413c641d9529e88a0c2a1296dfe6c7b9912
Nademtis <mathias.99.madsen@gmail.com>
2025-11-19 16:41:59 +0100

    player footsteps

--- game-off-2025-waves/scenes/player/player.gd
+++ game-off-2025-waves/scenes/player/player.gd
@@ -8,6 +8,7 @@ const MOVESPEED : float = 65
 @export var deceleration: float = 1000
 
 @onready var height_map: TileMapLayer = %heightMap
+@onready var walkable: TileMapLayer = %walkable
 
 @export var debug_mark_tile_under_player : bool = false
 var debug_tile_world_pos: Vector2 = Vector2.ZERO
@@ -19,6 +20,14 @@ var is_on_platform : bool = false
 # 2D input direction before isometric transform
 var input_dir: Vector2
 var move_dir: Vector2
+
+#sfx
+@onready var sfx_footstep_grass: AudioStreamPlayer2D = $sfxFootStep/sfx_footstep_grass
+@onready var sfx_footstep_stone: AudioStreamPlayer2D = $sfxFootStep/sfx_footstep_stone
+const SURFACE_TYPE_TILE_NAME : String = "surface_type"
+var footstep_cooldown := 0.0 # don't change - 
+var footstep_interval := 0.38 #the interval for howw often steps are played
+
 
 #var player_pos_node_to_update : Vector2 #used for follow camera's since this object is reparented
 #@onready var player_position_node: Node2D
@@ -47,12 +56,38 @@ func _process(_delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	_movement(delta)
+	_handle_footsteps(delta)
 	
 	if not is_on_platform: #platform change this bool
 		#if player is on platform. height should not be changed by player itself
 		_get_height_tile_under_player()
 	
+
+func _handle_footsteps(delta: float) -> void:
+	# Must be moving AND on the ground
+	if velocity.length() > 10.0: # threshold so tiny jitter doesn't trigger
+		footstep_cooldown -= delta
+
+		if footstep_cooldown <= 0.0:
+			_play_footstep()
+			footstep_cooldown = footstep_interval
+	else:
+		# Reset when not moving so it plays instantly on next step
+		footstep_cooldown = 0.0
+
+func _play_footstep() -> void:
+	var tile := walkable.get_cell_tile_data(height_map.local_to_map(height_map.to_local(global_position)))
 	
+	if tile:
+		var surface : Variant = tile.get_custom_data(SURFACE_TYPE_TILE_NAME)
+		
+		if surface == "grass":
+			sfx_footstep_grass.play()
+		else:
+			sfx_footstep_stone.play()
+	else: #still play even if data was not found - defaults to stone sfx
+		sfx_footstep_stone.play()
+
 func _movement(delta : float) -> void:
 	input_dir = Input.get_vector("left", "right", "up", "down")
 
