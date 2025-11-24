88cf77ae5ff7282a52613be4f74eb8cd21c4438b
Nademtis <mathias.99.madsen@gmail.com>
2025-11-24 18:49:40 +0100

    levelssss

--- game-off-2025-waves/scenes/main.gd
+++ game-off-2025-waves/scenes/main.gd
@@ -6,7 +6,7 @@ extends Node2D
 #const FIRST_LEVEL_PATH: String = "res://levels/level_playground.tscn"
 #const FIRST_LEVEL_PATH: String = "res://levels/level_template.tscn"
 
-const FIRST_LEVEL_PATH: String = "res://levels/level_7.tscn"
+const FIRST_LEVEL_PATH: String = "res://levels/level_9.tscn"
 
 
 var next_level_path: String
